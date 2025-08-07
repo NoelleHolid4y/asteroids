330ada14b330d9c806d5b6773964a05cb94f4a48
NoelleHolid4y <colletedoparaguai@gmail.com>
2025-08-07 20:20:15 -0300

    Add Score

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from player import *
 from asteroid import *
 from asteroidfield import *
 
+
 updatable = pygame.sprite.Group()
 drawable = pygame.sprite.Group()
 asteroids = pygame.sprite.Group()
@@ -21,10 +22,16 @@ Shot.containers = (bullets, updatable, drawable)
 spaceship = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 asteroidBelt = AsteroidField()
 
+
 def main():
     pygame.init()
+    pygame.display.set_caption("Asteroids!")
 
 def drawingScreen():
+    score = 0.0
+    terminalfont = pygame.font.SysFont("Terminal", 25)
+    
+
     while True:
         dt = timer.tick(60) / 1000
 
@@ -40,10 +47,16 @@ def drawingScreen():
                 if bullet.collides(asteroid):
                     bullet.kill()
                     asteroid.split()
+                    score += 1
+        
+        scoreText = terminalfont.render(f"Score: {score}", False, (255,255,255)) #False is for antialiasing
+        scoreRect = scoreText.get_rect()
+        scoreRect.center = ((SCREEN_WIDTH / 2, 25))
         
         for obj in drawable:
             obj.draw(screen)
-        
+        screen.blit(scoreText, scoreRect)
+
         pygame.display.flip()
 
         for event in pygame.event.get(): #quitting game
